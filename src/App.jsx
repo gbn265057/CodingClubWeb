9674c43f1983f7c0b47a365fc2a5798fd0194b35
Juhplux <68930642+Juhplux@users.noreply.github.com>
2024-11-04 12:48:23 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,8 @@ function App() {
     <Card link = "https://drive.google.com/file/d/1hPf2aDMfT5gDCOPNDj3ioyWxND4FGmYA/view" name = "9/16/24 - Paint the Town!" subtext="Painted a window in Downtown Northbrook for our club!"></Card>
     <Card name = "9/23/24 - Introduction to Python and Github" subtext="Created Collatz conjecture in python" link ="https://docs.google.com/presentation/d/1ZtsqtGZKWqgwvfEcOOa8sRXd1WmI4M7TcWcLamOOj1M/edit#slide=id.p"></Card>
     <Card name = "9/30/24 - First Python Project and More GitHub" subtext="Created dice game in python, first push in GitHub" link ="https://github.com/Juhplux/CodingclubMega_repo"></Card>
+    <Card name = "11/4/24 - Bebras Challenge!" subtext="Algorithms and other puzzles"></Card>
+
     </div>
     <div id = "idea">
       <Contact></Contact>
